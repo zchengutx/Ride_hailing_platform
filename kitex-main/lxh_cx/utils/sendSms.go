a8de87338bff13c472e9cb16d924cd65519e6320
xy644 <xy644055159@163.com>
2025-07-12 11:06:41 +0800

    修改

--- kitex-main/lxh_cx/utils/sendSms.go
+++ kitex-main/lxh_cx/utils/sendSms.go
@@ -1,50 +1,41 @@
 package utils
 
-import (
-	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
-	dysmsapi20170525 "github.com/alibabacloud-go/dysmsapi-20170525/v5/client"
-	util "github.com/alibabacloud-go/tea-utils/v2/service"
-	"github.com/alibabacloud-go/tea/tea"
-	credential "github.com/aliyun/credentials-go/credentials"
-	config2 "lxh_cx/config"
-)
-
 // Description:
 //
 // 使用凭据初始化账号Client
 //
 // @return Client
+////
+//// @throws Exception
+//func CreateClient() (_result *dysmsapi20170525.Client, _err error) {
+//	// 工程代码建议使用更安全的无AK方式，凭据配置方式请参见：https://help.aliyun.com/document_detail/378661.html。
+//	_, _err = credential.NewCredential(nil)
+//	if _err != nil {
+//		return _result, _err
+//	}
+//	config := &openapi.Config{
+//		// 您的AccessKey ID
+//		AccessKeyId: tea.String(config2.DataConfig.ALiYun.AccessKeyID),
+//		// 您的AccessKey Secret
+//		AccessKeySecret: tea.String(config2.DataConfig.ALiYun.AccessKeySecret),
+//	}
+//	// Endpoint 请参考 https://api.aliyun.com/product/Dysmsapi
+//	config.Endpoint = tea.String("dysmsapi.aliyuncs.com")
+//	_result = &dysmsapi20170525.Client{}
+//	_result, _err = dysmsapi20170525.NewClient(config)
+//	return _result, _err
+//}
 //
-// @throws Exception
-func CreateClient() (_result *dysmsapi20170525.Client, _err error) {
-	// 工程代码建议使用更安全的无AK方式，凭据配置方式请参见：https://help.aliyun.com/document_detail/378661.html。
-	_, _err = credential.NewCredential(nil)
-	if _err != nil {
-		return _result, _err
-	}
-	config := &openapi.Config{
-		// 您的AccessKey ID
-		AccessKeyId: tea.String(config2.DataConfig.ALiYun.AccessKeyID),
-		// 您的AccessKey Secret
-		AccessKeySecret: tea.String(config2.DataConfig.ALiYun.AccessKeySecret),
-	}
-	// Endpoint 请参考 https://api.aliyun.com/product/Dysmsapi
-	config.Endpoint = tea.String("dysmsapi.aliyuncs.com")
-	_result = &dysmsapi20170525.Client{}
-	_result, _err = dysmsapi20170525.NewClient(config)
-	return _result, _err
-}
-
-func SendSms(mobile, code string) (sms *dysmsapi20170525.SendSmsResponse, _err error) {
-	client, _err := CreateClient()
-	if _err != nil {
-		return sms, _err
-	}
-
-	sendSmsRequest := &dysmsapi20170525.SendSmsRequest{
-		PhoneNumbers:  &mobile,
-		TemplateParam: &code,
-	}
-	sms, _err = client.SendSmsWithOptions(sendSmsRequest, &util.RuntimeOptions{})
-	return sms, _err
-}
+//func SendSms(mobile, code string) (sms *dysmsapi20170525.SendSmsResponse, _err error) {
+//	client, _err := CreateClient()
+//	if _err != nil {
+//		return sms, _err
+//	}
+//
+//	sendSmsRequest := &dysmsapi20170525.SendSmsRequest{
+//		PhoneNumbers:  &mobile,
+//		TemplateParam: &code,
+//	}
+//	sms, _err = client.SendSmsWithOptions(sendSmsRequest, &util.RuntimeOptions{})
+//	return sms, _err
+//}
